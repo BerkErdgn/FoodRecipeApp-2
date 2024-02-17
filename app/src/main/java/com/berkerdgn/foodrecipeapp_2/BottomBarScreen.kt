ae11e8e695b5dd896fec58c9dfcd9e8c5d518b20
Berk Erdoğan <erdgnberk@gmail.com>
2024-02-17 23:33:19 +0300

    Splashscreen and NavigationBar
    splashscreen and navigationBar completed

---
+++ app/src/main/java/com/berkerdgn/foodrecipeapp_2/BottomBarScreen.kt
@@ -0,0 +1,31 @@
+package com.berkerdgn.foodrecipeapp_2
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class BottomBarScreen(
+    val route: String,
+    val title: String,
+    val icon: ImageVector,
+    val unselectedIcon: ImageVector
+){
+    object Home : BottomBarScreen(
+        route = "HOME",
+        title = "Home",
+        icon = Icons.Filled.Home,
+        unselectedIcon = Icons.Outlined.Home
+    )
+
+    object Save : BottomBarScreen(
+        route = "SAVE",
+        title = "Save",
+        icon = Icons.Filled.Favorite,
+        unselectedIcon = Icons.Outlined.Favorite
+    )
+
+}
---
+++ app/src/main/java/com/berkerdgn/foodrecipeapp_2/HomeScreen.kt
@@ -0,0 +1,146 @@
+package com.berkerdgn.foodrecipeapp_2
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.berkerdgn.foodrecipeapp_2.graphs.HomeNavGraph
+import com.berkerdgn.foodrecipeapp_2.ui.theme.customGray
+import com.berkerdgn.foodrecipeapp_2.ui.theme.customOrange
+
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun HomeScreen(navController: NavHostController = rememberNavController()) {
+
+    val screens = listOf(
+        BottomBarScreen.Home,
+        BottomBarScreen.Save
+    )
+    var bottomNavState by rememberSaveable {
+        mutableStateOf(0)
+    }
+
+    Scaffold (
+        //bottomBar = { BottomBar(navController = navController) }
+        bottomBar = {
+            NavigationBar(
+                modifier = Modifier
+                    .height(90.dp)
+                    .padding(8.dp)
+                    .clip(RoundedCornerShape(13.dp)),
+                containerColor = customOrange
+            ) {
+                screens.forEachIndexed { index, item ->
+
+                    NavigationBarItem(
+                        selected = bottomNavState == index,
+                        onClick = {
+                                  bottomNavState = index
+                                  navController.navigate(item.route){
+                                      popUpTo(navController.graph.findStartDestination().id)
+                                      launchSingleTop = true
+                                  }
+                        },
+                        icon = {
+                            Icon(
+                                imageVector = if (bottomNavState == index) item.icon else item.unselectedIcon ,
+                                contentDescription = item.title
+                            )
+                        },
+                        label = {
+                            Text(text = item.title)
+                        },
+                        colors = NavigationBarItemDefaults.colors(
+                            selectedIconColor = Color.Black,
+                            selectedTextColor = Color.Black,
+                            indicatorColor = Color(0xFFD86F00)
+                        )
+                    )
+                }
+            }
+        }
+    ){
+        HomeNavGraph(navController = navController)
+    }
+
+}
+
+
+
+
+/*
+@Composable
+fun BottomBar(navController: NavHostController) {
+    val screens = listOf(
+        BottomBarScreen.Home,
+        BottomBarScreen.Save
+    )
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+
+    val bottomBarDestination = screens.any { it.route == currentDestination?.route }
+    if (bottomBarDestination){
+        BottomNavigation {
+            screens.forEach { screen ->
+                AddItem(
+                        screen = screen,
+                    currentDestination = currentDestination,
+                    navController = navController
+                )
+            }
+        }
+    }
+
+}
+
+@Composable
+fun RowScope.AddItem(
+    screen: BottomBarScreen,
+    currentDestination: NavDestination?,
+    navController: NavHostController
+) {
+    BottomNavigationItem(
+        label = {
+            Text(text = screen.title)
+        },
+        icon = {
+            Icon(
+                imageVector = screen.icon,
+                contentDescription = "Navigation Icon"
+            )
+        },
+        selected = currentDestination?.hierarchy?.any{
+            it.route == screen.route
+        } == true,
+        unselectedContentColor = LocalContentColor.current.copy(alpha = ContentAlpha.disabled),
+        onClick = {
+            navController.navigate(screen.route){
+                popUpTo(navController.graph.findStartDestination().id)
+                launchSingleTop = true
+            }
+        }
+
+    )
+}
+ */
